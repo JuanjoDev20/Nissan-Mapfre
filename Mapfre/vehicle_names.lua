6c4b8ba1102574a21db6eaf590edf595cea1deeb
JuanjoDev20 <95863937+JuanjoDev20@users.noreply.github.com>
2022-06-11 18:57:23 +0200

    Add files via upload

---
+++ Mapfre/vehicle_names.lua
@@ -0,0 +1,12 @@
+Citizen.CreateThread(function()
+    AddTextEntry('sef_pfinder', 'Mapfre')
+
+
+end)
+
+
+author "JuanjoDev20"
+version "1.0"
+description "Version sin optimizar, la version optimizada de texturas la encuentras a 2.5 Euros. Mas informacion en el siguiente discord :https://discord.gg/Q5e4yGp6Sn "
+Credits "https://es.gta5-mods.com/vehicles/portuguese-sef-foreign-and-frontier-nissan-pathfinder-addon-livery"
+Credits "Alexbello3"
